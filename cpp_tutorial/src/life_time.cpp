c081cf2bdbe3cdcdb0961ec09b7b39d43703097d
gl w <2366813259@qq.com>
2022-12-07 21:03:28 +0800

    fix bug of life_time file

--- cpp_tutorial/src/life_time.cpp
+++ cpp_tutorial/src/life_time.cpp
@@ -20,7 +20,7 @@ public:
 
 class ScopePtr
 {
-private：
+private:
     Entity* m_ptr;
 public:
     ScopePtr(Entity* ptr) : m_ptr(ptr)
@@ -31,21 +31,21 @@ public:
         delete m_ptr;
     }
       
-}
-
+};
+/*
 int* CreateArray()  //useless
 {
     int array[50];  // local stack
     int* array1 = new array[50]; //heap
     return array;
 }
-
+*/
 void test_life_time()
 {
     {  //scope
         Entity e;   // stack  create and destroyed 
     }
-    Entity e1 = new Entity();  // heap
+    Entity* e1 = new Entity();  // heap
     delete e1;  //need destroyed manual;
   
     ScopePtr e2 = new Entity();  //delete
