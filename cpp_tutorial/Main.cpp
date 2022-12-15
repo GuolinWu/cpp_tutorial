9231d1e6cfb23ef309ac6d93b76238f15313eee5
Guolin Wu <43269389+GuolinWu@users.noreply.github.com>
2022-12-15 20:41:37 +0800

    Update Main.cpp

--- cpp_tutorial/Main.cpp
+++ cpp_tutorial/Main.cpp
@@ -6,6 +6,8 @@ int main()
 	std::cout << "Hello world" << std::endl;
 	//test_enum();
 	//test_life_time();
-	test_pointer();
+	//test_pointer();
+	//test_micro();
+	//test_func_pointer();
 	std::cin.get();
 }
